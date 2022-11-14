1d950da7662ea3859b0e624978e432f1df2a1480
s_suwijak <ssuwijak@hotmail.com>
2022-11-14 15:00:06 +0700

    feature upgrade on exam2

--- turing_exam2.py
+++ turing_exam2.py
@@ -1,23 +1,58 @@
 def isValid(s: str) -> bool:
-    ret = False
-
-    my_stack = 0
+    brackets = [0, 0, 0]
+    sum_stack = 0
 
     for x in s:
-        print(x, end='')
+        log(x, True, False)
 
-        if x == '(' or x == '{' or x == '[':
-            my_stack += 1
-        elif x == ')' or x == '}' or x == ']':
-            my_stack -= 1
+        if x == '(':
+            brackets[0] += 1
+        elif x == '{':
+            brackets[1] += 1
+        elif x == '[':
+            brackets[2] += 1
+        elif x == ')':
+            if brackets[0] < 1:
+                log("\n() closing out of order")
+                brackets[0] -= 1
+            brackets[0] -= 1
+        elif x == '}':
+            if brackets[1] < 1:
+                log("\n{} closing out of order")
+                brackets[1] -= 1
+            brackets[1] -= 1
+        elif x == ']':
+            if brackets[2] < 1:
+                log("\n[] closing out of order")
+                brackets[2] -= 1
+            brackets[2] -= 1
         else:
             pass
 
-    return my_stack == 0
+    if brackets[0] != 0:
+        log("\nfinally () not be closed completely")
+    elif brackets[1] != 0:
+        log("\nfinally {} not be closed completely")
+    elif brackets[2] != 0:
+        log("\nfinally [] not be closed completely")
+    else:
+        pass
+
+    sum_stack = brackets[0]+brackets[1]+brackets[2]
+
+    return sum_stack == 0
+
+
+def log(msg, debugMode=True, newline=True):
+    if debugMode:
+        if newline:
+            print(msg)
+        else:
+            print(msg, end='')
 
 
 if __name__ == "__main__":
-    line = "jkgljdfkslgfds()"
+    line = "jkgljdfkslgfds]([)"
 
     if isValid(line):
         print("Valid")
